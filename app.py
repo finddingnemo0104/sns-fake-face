cb9108381f80850c96453fbeea1c2ed9936bbe49
kungman24062003 <lenhkungman4344@gmail.com>
2024-11-19 02:24:51 +0700

    update login vs create register

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/login')
 def hello_world():  # put application's code here
     return render_template('login.html', title="My Flask App ", heading="Hello, Flask!")
+
+@app.route("/register")
+def register():
+    return render_template('register.html')  # Hiển thị file dangky.html
 
 
 if __name__ == '__main__':
